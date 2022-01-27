35350ec3b3ece05fcf3e9f679a32d18e4b7a0970
Stefizard <49508930+Stefizard@users.noreply.github.com>
2022-01-27 19:29:27 +0200

    Add project files.

---
+++ ProiectDAW/Models/Base/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectFinalDAW.Models.Base
+{
+    public class BaseEntity 
+    {
+
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime? DateCreated { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? DateModified { get; set; }
+    }
+}
